271d7fed68737ff62b9fd6d4093e3fb5e5a7c0f8
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2024-11-18 20:26:37 +0000

    genratorDomain

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let dominiosGenerados = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let e = 0; e < adj.length; e++) {
+      for (let u = 0; u < noun.length; u++) {
+        dominiosGenerados.push(pronoun[i] + adj[e] + noun[u] + ".com");
+      }
+    }
+  }
+
+  console.log(dominiosGenerados);
+  let preHtml = "";
+  let presentacion = "";
+  for (let i = 0; i < dominiosGenerados.length; i++) {
+    preHtml += "<li>" + dominiosGenerados[i] + "</li>";
+  }
+  presentacion = "<lu>" + preHtml + "</lu>";
+  document.getElementById("dominios").innerHTML = presentacion;
 };
